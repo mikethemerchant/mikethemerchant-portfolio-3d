6247b20028fea9f7b78579a061a7211ceeaa0cee
Michael Bender <mikethemerchant@gmail.com>
2023-08-10 12:35:07 -0400

    3222 navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from "../styles";
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+     >
+      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0)
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Michael Bender <span className="sm:block hidden"> | MikeTheMerchant</span></p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+            )) }
+
+        </ul>
+      </div>
+     </nav>
   )
 }
 
