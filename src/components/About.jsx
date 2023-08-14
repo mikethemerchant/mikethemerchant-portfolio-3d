f9b4eabe664a8faf2d6bd8d8bd17758fdab068f0
Michael Bender <mikethemerchant@gmail.com>
2023-08-14 11:28:06 -0400

    111 about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Overview.
+        </h2>
+      </motion.div>
+
+      <motion.p 
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+      >
+        I am a skilled problem software engineer with a passion for building.  
+        I have experiend in Javascript and experties in frameworks like React and Node.js.  
+        I am a quick learner and I am always looking to expand my skillset.  
+        The past years was in a microsoft shop and I am relearning a new stack. 
+      </motion.p>
+    </>
   )
 }
 
