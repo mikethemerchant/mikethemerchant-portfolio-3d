e65e00ad5794317d3964b43d1c115cd527032536
Michael Bender <mikethemerchant@gmail.com>
2023-08-15 10:24:56 -0400

    tilt working cards square

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,18 +15,16 @@ const ServiceCard = ({ index, title, icon }) => (
     <Tilt className="xs:w-[250] w-25 h-25 ">
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="w-full green-pink-gradient p-[2px] rounded-[35px] shadow-card"
       >
         <div
-          options={{ max: 25,
-                     scale: 1,
-                     speed: 450,     
-                   }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className="bg-tertiary rounded-[35px] py-2 px-2 min-h-[200px] min-w-[300px] 
+          flex justify-evenly items-center flex-col"
         >
           <img src={icon} 
                alt='web-development' 
-               className="w-16 h-16 object-contain" />
+               className="w-16 h-16 object-contain" 
+          />
           <h3 className="text-white text-[20px] font-bold text-center">
             {title}
           </h3>
